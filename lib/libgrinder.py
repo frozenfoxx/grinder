2d57521924cf4000ff44bb44966a29d61c7b557f
FOXX <siliconfoxx@gmail.com>
2016-06-19 15:47:32 -0500

    Update libgrinder.py

--- lib/libgrinder.py
+++ lib/libgrinder.py
@@ -4,8 +4,15 @@
 # Make directory structure for the branch
 # mkdir -p <branch>/{formulas,states,pillar}
 
+# Change into the branchdir
+# cd <branch>
+# branchdir = pwd
+
 # Grab zipfile of the branch
 # https://github.com/<user>/<repo>/archive/<branch>.zip
 
 # Expand in states
-# cd states ; unzip <branch>.zip ; cd ..
+# cd states ; unzip <branch>.zip
+
+# Cleanup
+# cd branchdir ; rm <branch>.zip
