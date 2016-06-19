0ec55eefe2850d1d1730063537f2c9351f590b72
FOXX <siliconfoxx@gmail.com>
2016-06-19 15:46:02 -0500

    Create libgrinder.py

---
+++ lib/libgrinder.py
@@ -0,0 +1,11 @@
+# Change directory to the environments directory
+# cd <basedir>/environments/
+
+# Make directory structure for the branch
+# mkdir -p <branch>/{formulas,states,pillar}
+
+# Grab zipfile of the branch
+# https://github.com/<user>/<repo>/archive/<branch>.zip
+
+# Expand in states
+# cd states ; unzip <branch>.zip ; cd ..
