687f01b6f59b494744af361d1f84b553d31dc3ec
FOXX <siliconfoxx@gmail.com>
2016-06-25 14:27:55 -0500

    Added configuration file

---
+++ grinder/__init__.py
@@ -0,0 +1,1 @@
+from .libgrinder import *
---
+++ grinder/libgrinder.py
@@ -0,0 +1,18 @@
+# Change directory to the environments directory
+# cd <basedir>/environments/
+
+# Make directory structure for the branch
+# mkdir -p <branch>/{formulas,states,pillar}
+
+# Change into the branchdir
+# cd <branch>
+# branchdir = pwd
+
+# Grab zipfile of the branch
+# https://github.com/<user>/<repo>/archive/<branch>.zip
+
+# Expand in states
+# cd states ; unzip <branch>.zip
+
+# Cleanup
+# cd branchdir ; rm <branch>.zip
