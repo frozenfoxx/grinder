2cdf4b89e413a00860fcc4caa41c6d2e0095fd57
FOXX <siliconfoxx@gmail.com>
2016-07-08 23:12:19 -0500

    Syntax change

--- grinder/seasoning.py
+++ grinder/seasoning.py
@@ -5,17 +5,17 @@ import os
 import yaml
 
 class Seasoning:
-  def __init__(basedir, branch):
+  def __init__(self, basedir, branch):
     self.fileref = basedir + "/" + branch + "/states/seasoning.yml"
     self.statesdir = basedir + "/" + branch + "/states"
     self.config = ""
 
-  def loadseasoning():
+  def loadseasoning(self):
     stream = open(self.fileref)
     self.config = yaml.safe_load(stream)
     stream.close()
   
-  def cloneformula(name, url, branch):
+  def cloneformula(self, name, url, branch):
     targetdir = self.statesdir + "/" + name
     g = git.cmd.Git()
     g.clone(url, targetdir, branch=branch, depth=1)
