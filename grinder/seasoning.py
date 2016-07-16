895afe480ab573ec50e67c618b67e613d5b18677
FOXX <siliconfoxx@gmail.com>
2016-07-15 23:34:51 -0500

    Updated seasoning.py

--- grinder/seasoning.py
+++ grinder/seasoning.py
@@ -10,19 +10,23 @@ class Seasoning:
     self.fileref = self.statesdir + "/seasoning.yml"
     self.config = ""
 
-  def deploy(self):
-    self.loadseasoning()
-    for key in self.config:
-      print("Found key " + key + " in the seasoning.yml")
-
   def loadseasoning(self):
     stream = open(self.fileref)
     self.config = yaml.safe_load(stream)
     stream.close()
+
+  def deploy(self):
+    self.loadseasoning()
+    for key in self.config:
+      print("Deploying " + key + " formula...")
+      self.cloneformula(key, self.config[key]['reponame'], self.config[key]['url'], self.config[key]['branch'])
   
-  def cloneformula(self, name, url, branch):
-    targetdir = self.statesdir + "/" + name
+  def cloneformula(self, formulaname, reponame, url, branch):
+    repotargetdir = self.statesdir + "/" + reponame
+    clonedformuladir = repotargetdir + "/" + formulaname
+
     g = git.cmd.Git()
-    g.clone(url, targetdir, branch=branch, depth=1)
-    # mv the cloned target/reponame -> self.statesdir
-    # shutil.rmtree(reponame)
+    g.clone(url, repotargetdir, branch=branch, depth=1)
+
+    shutil.move(clonedformuladir, (self.statesdir + "/" + formulaname))
+    shutil.rmtree(repotargetdir)
