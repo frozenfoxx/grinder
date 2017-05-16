d7d734e3e7c7502e361bf5f44e6262e922aeefae
FOXX <siliconfoxx@gmail.com>
2017-05-16 00:43:26 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'grinder',
+    packages = ['grinder'],
+    version = '0.1',
+    description = 'Dynamic environment deployment tool for SaltStack',
+    author = 'FrozenFOXX',
+    author_email = 'siliconfoxx@gmail.com',
+    url = 'https://github.com/frozenfoxx/grinder',
+    download_url = 'https://github.com/frozenfoxx/grinder/archive/0.1.tar.gz',
+    keywords = ['salt', 'devops', 'deployment', 'pillar'],
+    classifiers = [],
+)
