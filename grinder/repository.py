85bcd01b83c0abb639df44f91b7fc3b09f3f604a
FOXX <siliconfoxx@gmail.com>
2017-05-25 01:07:16 -0500

    Added Repository class

---
+++ grinder/repository.py
@@ -0,0 +1,18 @@
+class Repository:
+    def __init__(self, url, basedir, postfix):
+        self.url = url
+        self.basedir = basedir
+        self.postfix = postfix
+
+    def lsremote(self):
+        remote_refs = {}
+        g = git.cmd.Git()
+        for ref in g.ls_remote(self.url).split('\n'):
+            hash_ref_list = ref.split('\t')
+            remote_refs[hash_ref_list[1]] = hash_ref_list[0]
+        return remote_refs
+
+    def clone(self, branch):
+        targetdir = self.basedir + "/" + branch + "/" + self.postfix
+        g = git.cmd.Git()
+        g.clone(self.url, targetdir, branch=branch, depth=1)
