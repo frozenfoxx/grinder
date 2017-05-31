cf1d265e14fce44341af3a7c4200b3955b2903ed
FOXX <siliconfoxx@gmail.com>
2017-05-31 00:56:24 -0500

    Updated setup and grinder script

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
+""" Grinder packaging instructions """
+
 from distutils.core import setup
 
 setup(
-    name = 'grinder',
-    packages = ['grinder'],
-    version = '0.1',
-    description = 'Dynamic environment deployment tool for SaltStack',
-    author = 'FrozenFOXX',
-    author_email = 'siliconfoxx@gmail.com',
-    url = 'https://github.com/frozenfoxx/grinder',
-    download_url = 'https://github.com/frozenfoxx/grinder/archive/0.1.tar.gz',
-    keywords = ['salt', 'devops', 'deployment', 'pillar'],
-    classifiers = [],
-    scripts = [
+    name='grinder',
+    packages=['grinder'],
+    version='0.1',
+    description='Dynamic environment deployment tool for SaltStack',
+    author='FrozenFOXX',
+    author_email='siliconfoxx@gmail.com',
+    url='https://github.com/frozenfoxx/grinder',
+    download_url='https://github.com/frozenfoxx/grinder/archive/0.1.tar.gz',
+    keywords=['salt', 'devops', 'deployment', 'pillar'],
+    classifiers=[],
+    scripts=[
         'scripts/grinder',
+    ],
+    data_files=[
+        ('/etc/grinder', ['conf/grinder.conf'])
     ],
 )
